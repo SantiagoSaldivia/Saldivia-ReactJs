23f6184ea5c9e90f031d1f380d03080c2a1dcafa
SantiagoSaldivia <saldiviasantiago7@gmail.com>
2023-09-21 19:41:57 -0300

    Primera Entrega Saldivia

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const data = [
+	{
+		id: 1,
+		img : '',
+		nameProduct: 'Remera De River Original',
+		price: 150,
+		quantity: 1,
+	},
+	{
+		id: 2,
+		img: '',
+		nameProduct: 'Remera de Argentina 2022 Qatar',
+		price: 400,
+		quantity: 1,
+	},
+	{
+		id: 3,
+		img: '',
+		nameProduct: 'Remera de Brasil 1998',
+		price: 300,
+		quantity: 1,
+	},
+	{
+		id: 4,
+		img: '',
+		nameProduct: 'Remera de Mèxico 1970',
+		price: 100,
+		quantity: 1,
+	},
+	{
+		id: 5,
+		img: '',
+		nameProduct: 'Remera de Brasil 2002',
+		price: 350,
+		quantity: 1,
+	},
+	
+];
